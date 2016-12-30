156126838ae3c9f6d24a33a8a377c37580a0921c
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2016-12-30 23:18:13 +0400

    improving service worker

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,20 @@ gulp.task('generate-service-worker', function(callback) {
 
   swPrecache.write(path.join(rootDir, 'sw.js'), {
     staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+    runtimeCaching: [
+    	{
+	    	urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+	    	handler: 'cacheFirst'
+    	},
+    	{
+    		urlPattern: /^https:\/\/www\.google-analytics\.com\//,
+	    	handler: 'cacheFirst'
+    	},
+    	{
+    		urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+    		handler: 'cacheFirst'
+    	}
+    ],
     stripPrefix: rootDir
   }, callback);
 });
